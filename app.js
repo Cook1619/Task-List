a12a89f918a8b02d2f37e8757e2366df9196e0c3
Cook1619 <cook6348@gmail.com>
2018-10-05 12:39:45 -0500

    Adding tasks to list working as intended

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
-let val;
+//Define UI Vars
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
 
-val = document.all;
+//Load all event listeners
+loadEventListeners();
 
-console.log(val);
+function loadEventListeners() {
+  //add task event
+  form.addEventListener("submit", addTask);
+}
+
+//Add Task
+function addTask(e) {
+  if (taskInput.value === "") {
+    alert("Add a task.");
+  }
+  //Create li element
+  const li = document.createElement("li");
+  li.className = "collection-item";
+  //create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  //Create new link element
+  const link = document.createElement("a");
+  link.className = "delete-item secondary-content";
+  //Add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  //Append the link to li
+  li.appendChild(link);
+  //Append li to the ul
+  taskList.appendChild(li);
+  //Clear input
+  taskInput.value = "";
+  e.preventDefault();
+}
